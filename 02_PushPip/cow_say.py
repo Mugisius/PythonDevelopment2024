fd83963129ca04aa1ccf4edce096002d0df2ab7c
Akinin Evgeniy <olympiads@inbox.ru>
2024-02-26 22:13:07 +0300

    Implemented -n option

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -7,6 +7,7 @@ parser.add_argument("message", nargs="*", type=str)
 parser.add_argument('-e', default=cowsay.Option.eyes, type=str)
 parser.add_argument("-f", dest="file", action="store", default="default", type=str)
 parser.add_argument("-l", action="store_true")
+parser.add_argument("-n", action="store_false")
 args = parser.parse_args()
 
 if args.l:
@@ -15,9 +16,12 @@ if args.l:
     exit(0)
 
 if args.message:
+    if not args.n:
+        parser.print_help()
+        exit(0)
     message = [w for arg in args.message for w in arg.split(" ") if w]
     message = " ".join(message)
 else:
     message = "".join(sys.stdin.readlines())
 
-print(cowsay.cowsay(message,eyes=args.e[:2],cow=args.file))
+print(cowsay.cowsay(message,eyes=args.e[:2],cow=args.file,wrap_text=args.n))
