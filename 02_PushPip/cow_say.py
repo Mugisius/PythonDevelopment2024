e84e635c36fa75e3dfce0d804ed80c13ecb4da2b
Akinin Evgeniy <olympiads@inbox.ru>
2024-02-26 22:40:31 +0300

    Implemented -bdgpstwy options

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -10,12 +10,25 @@ parser.add_argument("-l", action="store_true")
 parser.add_argument("-n", action="store_false")
 parser.add_argument("-T", type=str, default=cowsay.Option.tongue)
 parser.add_argument("-W", type=int, default=40)
+
+parser.add_argument("-b", const="b", dest="preset", action="append_const")
+parser.add_argument("-d", const="d", dest="preset", action="append_const")
+parser.add_argument("-g", const="g", dest="preset", action="append_const")
+parser.add_argument("-p", const="p", dest="preset", action="append_const")
+parser.add_argument("-s", const="s", dest="preset", action="append_const")
+parser.add_argument("-t", const="t", dest="preset", action="append_const")
+parser.add_argument("-w", const="w", dest="preset", action="append_const")
+parser.add_argument("-y", const="y", dest="preset", action="append_const")
+
 args = parser.parse_args()
 
 if args.l:
     print("Cow files:")
     print(" ".join(sorted(cowsay.list_cows())))
     exit(0)
+
+if args.preset:
+    args.preset = args.preset[-1]
 
 if args.message:
     if not args.n:
@@ -32,5 +45,6 @@ print(cowsay.cowsay(
         cow=args.file,
         wrap_text=args.n, 
         tongue=args.T[:2], 
-        width=(args.W-1)
+        width=(args.W-1),
+        preset=args.preset
         ))
